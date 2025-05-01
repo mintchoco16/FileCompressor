c68efd28cefd50e85ab9f547df3802d2ebcf5162
mintchoco16 <mint.choco.16@gmail.com>
2025-05-01 12:56:54 -0400

    Pushing the project code with ExecutorService MultiThreading!

--- src/kaur/siimar/client/FileDetails.java
+++ src/kaur/siimar/client/FileDetails.java
@@ -3,21 +3,38 @@ package kaur.siimar.client;
 public class FileDetails {
 
     //once we have input, we save all these into FileDetails class.
-    //attributes we should have: name of file, the string file path, [the 4 parameters from google doc]
 
-//    private String filePath; // for file path
-//    private String algoName; // for the algorithm type the user wishes to choose
-//    private String threadType; // for the thread type the user would choose
-//    private File fileDet; // for all file details
-
-    String path;
-    int size;
-    String content;
-    // later make it private
+    private String path;
+    private int size;
+    private String content;
 
     public FileDetails(String path, int size, String content) {
         this.path = path;
         this.size = size;
         this.content = content;
+    }
+
+    public int getSize() {
+        return this.size; // cannot access it beyond this class because private attribute
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
--- src/kaur/siimar/client/InputParser.java
+++ src/kaur/siimar/client/InputParser.java
@@ -3,6 +3,8 @@ package kaur.siimar.client;
 import kaur.siimar.processor.NormalFileProcessingService;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputParser {
@@ -12,7 +14,7 @@ public class InputParser {
     // to put all the details into File object- so to validate and parse the input data this class is used
 
 
-    public static FileDetails inputParser() {
+    public static List<FileDetails> inputParser() {
         System.out.println("Your input is being loaded, kindly wait a moment... :)");
 
         Scanner inputScanner = new Scanner(System.in);
@@ -23,11 +25,13 @@ public class InputParser {
         inputScanner.nextLine();
         System.out.println("Enter the path: ");
         String fPath = inputScanner.nextLine();
+        List<FileDetails> listOfFileDetails = new ArrayList<>(); // use ArrayList cus don't want to specify size bcs unsure of number of elts we'll receive
 
-        
         if (optionSelect == 1) {
             FileDetails processedFile = fileProcObj.readFile(fPath); // don't use getPath() here because dealing directly with string input of file path
-            System.out.println("Here is the entered path: "+processedFile.path +", size of file: " +processedFile.size);
+            int fileOrigSize = processedFile.getSize() * 8;
+            System.out.println("Here is the entered path: "+processedFile.getPath() +", size of file: " + fileOrigSize);
+            listOfFileDetails.add(processedFile);
         } else if (optionSelect == 2) {
             File folder = new File(fPath);
             File[] listOfFiles = folder.listFiles();
@@ -37,14 +41,15 @@ public class InputParser {
                 System.out.println("Currently Reading File with Path: " +f.getPath());
                 FileDetails processedFile = fileProcObj.readFile(f.getPath()); // using this here because File[] would make all folder contents as objects
                 // making the object here so that we can also store the file name and path instead of just having the content because that will help in knowing what to return to which file and where the current content came from instead of having ambiguity when we just get the content returned and don't have an object storing/giving that information.
-                System.out.println("Here is the entered path: "+processedFile.path +", size of file: " +processedFile.size);
+                System.out.println("Here is the entered path: "+processedFile.getPath() +", size of file: " +processedFile.getSize());
+                listOfFileDetails.add(processedFile);
             }
 
         } else {
             System.out.println("Invalid Input, please enter either '1' or '2'.");
         }
 
-        return null;
+        return listOfFileDetails;
     }
 
 
